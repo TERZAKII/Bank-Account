3d4a998606e87269162e71e8572253c878f81302
TERZAKII <61076947+TERZAKII@users.noreply.github.com>
2023-05-03 16:52:30 +0600

    Bank Account

---
+++ BankAccount.java
@@ -0,0 +1,22 @@
+package PA;
+
+public class BankAccount {
+    private int balance;
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    public synchronized int getBalance() {
+        return balance;
+    }
+
+    public synchronized void deposit(int amount) {
+        balance += amount;
+    }
+
+    public synchronized void withdraw(int amount) {
+        balance -= amount;
+    }
+}
+
